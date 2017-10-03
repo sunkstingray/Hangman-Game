5e6750ed770e266dd5202f350cfde69309b5f438
unknown <lynam.chris@gmail.com>
2017-10-03 18:22:45 -0500

    added js functionality, added starting css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 // set list of words
-var hangWords = ["rifle", "horse", "revolver", "outlaw"];
+var hangWords = ["rifle", "horse", "revolver", "outlaw", "gunslinger", "saddle", "poncho", "treasure", "reward", "spurs", "boots", "noose", "horseshoe"];
 
 // set current word
 
@@ -17,6 +17,7 @@ var guessLeft = 12;
 var hangDisplayString = "";
 var hangDisplayFinal = "";
 var hangDisplayString = "";
+var numWins = 0;
 
 // convert the words to underscores
 
@@ -29,21 +30,36 @@ var hangDisplayString = "";
 
    return underScore;
  };
-// set initial blank underscores
+// set initial blank underscores and initial score
 
 hangDisplayString = hangSwap().toString();
 hangDisplayFinal = hangDisplayString.split(',').join(" ");
 document.getElementById("displayWord").innerHTML = hangDisplayFinal;
+document.getElementById("wins").innerHTML = numWins;
 
 // capture keystrokes and analyze
 
 document.onkeyup = function(event) {
   var userGuess = event.key;
 
+	if (userGuess.search(/[^a-zA-Z]+/) === -1) {
+		userGuess = userGuess.toLowerCase();
   if(rightWord.indexOf(userGuess) === -1 && wrongWord.indexOf(userGuess) === -1){
   	if(currentWord.indexOf(userGuess) > -1) {
   	rightWord.push(userGuess);
   	document.getElementById("displayWord").innerHTML = letterPosition(userGuess);
+  	if (hangDisplayFinal.split(' ').join('') === currentWord) {
+  		console.log(hangDisplayFinal);
+		numWins = numWins + 1;
+		document.getElementById("wins").innerHTML = numWins;
+		document.getElementById("displayWord").innerHTML = hangDisplayFinal;
+		alert("Well done! You Win! The word was    "+hangDisplayFinal+"   . Click OK to continue.");
+		currentWord = hangWords[Math.floor(Math.random() * hangWords.length)];
+		hangDisplayString = hangSwap().toString();
+		hangDisplayFinal = hangDisplayString.split(',').join(" ");
+		document.getElementById("displayWord").innerHTML = hangDisplayFinal;
+		guessLeft = 12;
+}
   }
   else {
   	wrongWord.push(userGuess);
@@ -53,10 +69,15 @@ document.onkeyup = function(event) {
   	guessLeft = guessLeft - 1;
   	document.getElementById("guessText").innerHTML = guessLeft;
   	if(guessLeft === 0){
-  		alert("You Lose!")
+  		alert("You Lose! Click OK to play again.")
+  		currentWord = hangWords[Math.floor(Math.random() * hangWords.length)];
+		hangDisplayString = hangSwap().toString();
+		hangDisplayFinal = hangDisplayString.split(',').join(" ");
+		document.getElementById("displayWord").innerHTML = hangDisplayFinal;
   	}
   	}
 	}
+}
 }
 
 var hangDisplay = hangSwap();
@@ -88,3 +109,11 @@ var hangDisplay = hangSwap();
 
 
 // reset on win or lose
+
+function onWin() {
+	alert("Well done! You Win! Click OK to continue.");
+	numWins = numWins + 1;
+	document.getElementById("wins").innerHTML = numWins;
+	currentWord = hangWords[Math.floor(Math.random() * hangWords.length)];
+	document.getElementById("displayWord").innerHTML = hangDisplayFinal;
+}
